5662e6c4343b0998d1a7b76cfeb599fb0f97ade8
FernandoYa <47068097+FernandoYa@users.noreply.github.com>
2023-03-26 16:25:31 +0100

    V3

--- main-side/main-master.js
+++ main-side/main-master.js
@@ -24,6 +24,16 @@ let DelteConfirm=document.querySelectorAll(".btnDel");
 DelteConfirm.forEach(el=> {
     el.addEventListener("click",()=> {
         myDelete.classList.add("hiddeDelete");
+})
+})
+let myNotify=document.querySelector(".notify");
+let myNotfiyButton=document.querySelectorAll(".notifyMe");
+
+myNotfiyButton.forEach(el=> {
+    el.addEventListener("click",()=> {
+        myNotify.style.cssText="display:flex"
     })
 })
-
+document.querySelector(".close").addEventListener("click",()=> {
+    myNotify.style.cssText="display:none"
+})
